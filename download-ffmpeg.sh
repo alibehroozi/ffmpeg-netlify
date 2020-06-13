8aa362b49f94dc881389dff4f4a7b8911a642b62
Ali Behroozi <alibehroozi@Alis-MacBook-Air.local>
2020-06-14 03:07:52 +0430

    xf

--- download-ffmpeg.sh
+++ download-ffmpeg.sh
@@ -2,7 +2,7 @@
 
 wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 
-tar -xzvf ffmpeg-release-amd64-static.tar.xz
+tar -xf ffmpeg-release-amd64-static.tar.xz
 
 cd ffmpeg-release-amd64-static
 
--- functions/ffmpeg.js
+++ functions/ffmpeg.js
@@ -1,7 +1,4 @@
-const ffmpeg = require("@ffmpeg-installer/ffmpeg");
-
 exports.handler = () => {
-  console.log(ffmpeg);
   return {
     statusCode: 200,
     body: "hi",
