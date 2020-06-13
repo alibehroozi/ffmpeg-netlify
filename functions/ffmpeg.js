7da8c23baea1a75a4d63a710236cf09ee9492ebf
Ali Behroozi <alibehroozi@Alis-MacBook-Air.local>
2020-06-14 04:20:28 +0430

    use ffmpeg -v spawn

--- functions/ffmpeg.js
+++ functions/ffmpeg.js
@@ -1,6 +1,12 @@
-exports.handler = () => {
-  return {
-    statusCode: 200,
-    body: "hi",
-  };
+const { spawn } = require("child_process");
+
+exports.handler = (event, context, callback) => {
+  const ffmpeg = spawn("../ffmpeg", ["-v"]);
+  ffmpeg.stderr.setEncoding("utf8");
+  ffmpeg.stdout.on("data", (data) => {
+    console.log(data);
+  });
+  ffmpeg.on("close", () => {
+    callback(null, { statusCode: 200, body: "hi" });
+  });
 };
