edf1ddde271e0dc1885ccc8c6d19bd0d0ae5caa4
Ali Behroozi <alibehroozi@Alis-MacBook-Air.local>
2020-06-14 02:49:15 +0430

    download ffmpeg

---
+++ download-ffmpeg.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+
+tar -xzvf ffmpeg-release-amd64-static.tar.xz
+
+cd ffmpeg-release-amd64-static
+
+ls
