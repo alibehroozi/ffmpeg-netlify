9771b477fca7ac6c3e48767f31c3bd453b763973
Ali Behroozi <alibehroozi@Alis-MacBook-Air.local>
2020-06-13 15:26:38 +0430

    add . for cmake

--- build.sh
+++ build.sh
@@ -419,7 +419,7 @@ make install
 
 echo "*** Building frei0r ***"
 cd $BUILD_DIR/frei0r*
-PATH="$BIN_DIR:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$TARGET_DIR" ..
+PATH="$BIN_DIR:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$TARGET_DIR" .
 make
 make install
 
